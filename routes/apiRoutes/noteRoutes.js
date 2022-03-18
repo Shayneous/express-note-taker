2a67f8f275dcd33f29985799fdf6479bf3855b84
The Arcterion Project <shaynemganness@gmail.com>
2022-03-17 21:34:12 -0400

    edit note routes and functions to allow notes to be deleted

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,18 +4,27 @@ const {
 } = require('../../db/db');
 const {
     makeNewNote,
+    deleteNote
 } = require('../../lib/noteFunctions');
 
-
+//Get note
 router.get('/notes', (req, res) => {
     let save = notes;
     res.json(save);
 })
 
+//Post note
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = makeNewNote(req.body, notes);
     res.json(note);
 })
+
+//Delete note
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
+
 
 module.exports = router;
